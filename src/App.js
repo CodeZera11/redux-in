21c054bd11590a6e1901c8fa40f2d3508c156e2b
Bhavesh Yadav <codezera3@gmail.com>
2023-06-26 00:10:43 +0530

    redux toolkit addition and basic counter application completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useSelector, useDispatch} from 'react-redux';
+import {actions} from './store/index'
 
 function App() {
 
@@ -7,15 +8,15 @@ function App() {
   const dispatch = useDispatch();
 
   const increment = ()=> {
-    dispatch({type: 'INC'});
+    dispatch(actions.increment());
   }
 
   const decrement = ()=> {
-    dispatch({type: 'DEC'})
+    dispatch(actions.decrement())
   }
 
   const addBy = ()=> {
-    dispatch({type: "ADD", payload: 10})
+    dispatch(actions.addBy(20))
   }
 
   return (
